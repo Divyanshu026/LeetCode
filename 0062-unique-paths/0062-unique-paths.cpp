01c7e859dabc35f3f8b4eca4686eda6d212850af
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-10 14:04:24 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -8,7 +8,18 @@ public:
         return dp[i][j] =  left + up;
     }
     int uniquePaths(int n, int m) {
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,dp);
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return f(n-1,m-1,dp);
+         vector<vector<int>> dp(n,vector<int>(m,0));
+         dp[0][0] = 1;
+         for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(i==0 && j==0) continue;
+                int left = i>0 ? dp[i-1][j] : 0;
+                int up = j>0 ? dp[i][j-1] : 0;
+                dp[i][j] =  left + up;
+            }
+         }
+         return dp[n-1][m-1];
     }
 };
