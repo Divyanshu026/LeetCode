3f34dd1019e765aba6d8648c57a389ca26dd5016
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-10 10:47:59 +0530

    Time: 27 ms (42%), Space: 139 MB (92.11%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,8 +5,9 @@ public:
         int n = nums.size();
         vector<int> res;
         for(int i=0; i<n; i++) {
-            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
+            
             while(!dq.empty() && nums[i]>nums[dq.back()]) dq.pop_back();
+            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
             dq.push_back(i);
             if(i>=k-1) res.push_back(nums[dq.front()]);
         }
