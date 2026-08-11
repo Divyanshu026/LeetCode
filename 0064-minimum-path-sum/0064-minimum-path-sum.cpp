70323045ae012f300367838cdafaaf25acc282a0
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-11 16:35:57 +0530

    Time: 0 ms (100%), Space: 15.4 MB (86.04%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,17 +13,18 @@ public:
         int m = grid[0].size();
         // vector<vector<int>> dp(n,vector<int>(m,-1));
         // return f(n-1,m-1,grid,dp);
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        dp[0][0] = grid[0][0];
+        // vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<int> dp(m,0);
+        dp[0] = grid[0][0];
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(i==0 && j==0) continue;
-                int left = j>0 ? grid[i][j] + dp[i][j-1] : 1e9;
-                int up = i>0 ? grid[i][j] + dp[i-1][j] : 1e9;
-                dp[i][j] = min(left,up);
+                int left = j>0 ? grid[i][j] + dp[j-1] : 1e9;
+                int up = i>0 ? grid[i][j] + dp[j] : 1e9;
+                dp[j] = min(left,up);
             }
         }
-        return dp[n-1][m-1];
+        return dp[m-1];
 
     }
 };
