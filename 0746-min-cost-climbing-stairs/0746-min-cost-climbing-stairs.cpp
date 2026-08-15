ad129f1ed59478af8292509cfbebd79b4faa2e62
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-15 15:46:49 +0530

    Time: 0 ms (100%), Space: 17.8 MB (30.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -10,10 +10,23 @@ public:
         int pick2 = nums[i] + f(i+2,nums,dp);
         return dp[i] = min(pick1,pick2);
     }
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp1(n+1,-1);
-        vector<int> dp2(n+1,-1);
-        return  min(f(0,cost,dp1),f(1,cost,dp2));
+    int minCostClimbingStairs(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp1(n+1,0);
+        vector<int> dp2(n+1,0);
+        // return  min(f(0,cost,dp1),f(1,cost,dp2));
+        dp1[n-1] = nums[n-1];
+        dp2[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--) {
+            int pick1 = nums[i] + dp1[i+1];
+            int pick2 = nums[i] + dp1[i+2];
+            dp1[i] = min(pick1,pick2);
+        }
+        for(int i=n-2; i>=1; i--) {
+            int pick1 = nums[i] + dp2[i+1];
+            int pick2 = nums[i] + dp2[i+2];
+            dp2[i] = min(pick1,pick2);
+        }
+        return min(dp1[0],dp2[1]);
     }
 };
