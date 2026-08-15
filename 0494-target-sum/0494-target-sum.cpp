d336501effb29d9c50f08fefd5c1b2a34e6a5d09
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-15 15:02:39 +0530

    Time: 277 ms (26.84%), Space: 11.7 MB (89.6%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& nums) {
+    int f(int i, int val, vector<int>& nums) {
         // base case
+        
         if(i==0) {
-            int add = j-nums[0]==0 ? 1 : 0;
-            int sub = j+nums[0]==0 ? 1 : 0;
-            return add + sub;
+            if(val==0) return nums[0]==0 ? 2 : 1;
+            else return val-nums[0]==0 ? 1 : 0;
         }
-        int add = f(i-1,j-nums[i],nums);
-        int sub = f(i-1,j+nums[i],nums);
-        return add + sub;
+         
+        int pick = f(i-1,val-nums[i],nums);
+        int notpick = f(i-1,val,nums);
+        return pick+notpick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        return f(n-1,target,nums);
+        int total = accumulate(nums.begin(),nums.end(),0);
+        if((target+total)%2!=0) return 0;
+        int x = (target + total)/2;
+        // vector<vector<int>> dp(n,vector<int>(x+1,-1));
+        return f(n-1,x,nums);
     }
 };
