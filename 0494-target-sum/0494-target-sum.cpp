123d62abfccf2f0443683494d01a8595bf49d1d5
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-15 15:08:17 +0530

    Time: 4 ms (76.88%), Space: 15.5 MB (45.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int f(int i, int val, vector<int>& nums) {
+    int f(int i, int val, vector<int>& nums,vector<vector<int>>& dp) {
         // base case
-        
+        if(val<0) return 0;
         if(i==0) {
             if(val==0) return nums[0]==0 ? 2 : 1;
-            else return val-nums[0]==0 ? 1 : 0;
+            else return val==nums[0];
         }
          
-        int pick = f(i-1,val-nums[i],nums);
-        int notpick = f(i-1,val,nums);
-        return pick+notpick;
+        if(dp[i][val]!=-1) return dp[i][val];
+        int pick = f(i-1,val-nums[i],nums,dp);
+        int notpick = f(i-1,val,nums,dp);
+        return dp[i][val] =  pick+notpick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int total = accumulate(nums.begin(),nums.end(),0);
+        if(abs(target) > total) return 0;
         if((target+total)%2!=0) return 0;
         int x = (target + total)/2;
-        // vector<vector<int>> dp(n,vector<int>(x+1,-1));
-        return f(n-1,x,nums);
+        vector<vector<int>> dp(n,vector<int>(x+1,-1));
+        return f(n-1,x,nums,dp);
     }
 };
