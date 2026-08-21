cc3fdb13820e47f838ebcd8afef3327145edad58
Divyanshu Shukla <divyanshu.dev026@gmail.com>
2026-08-21 08:41:33 +0530

    Time: 7 ms (96.21%), Space: 9 MB (86.32%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -20,11 +20,8 @@ public:
         // for(int i=0; i<n+1; i++) dp[i][0] = 1;
         dp[0]=1;
         for(int i=1; i<n+1; i++) {
-            vector<double> prev=dp;
-            for(int j=1; j<m+1; j++) {
-                double pick = s[i-1]==t[j-1] ? prev[j-1] : 0;
-                double notpick = prev[j];
-                dp[j] =  pick+notpick;
+            for(int j=m; j>=1; j--) {
+                if(s[i-1]==t[j-1]) dp[j] = dp[j-1] + dp[j];
             }
         }
         return (int)dp[m];
